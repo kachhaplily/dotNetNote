41afbe5948695047998961b7f526d0dd2e51717b
kachhaplily <kachhaplily@gmail.com>
2024-09-03 12:08:06 +0530

    Add tryParse function

---
+++ FirstApp/forloop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    internal class forloop
+    {
+        public forloop()
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+
+            // Iterate through the array
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            while (numbers[numbers.Length - 1] > 0)
+            {
+                Console.WriteLine("while loop");
+            }
+        }
+    }
+}
---
+++ FirstApp/tryparse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    internal class tryparse
+    {
+        public tryparse()
+        {
+            string value = Console.ReadLine();
+
+            int number;
+            double doubleNumber;
+
+            bool status= int.TryParse(value, out number);
+            bool doubleStatus = double.TryParse(value,out doubleNumber);
+
+            if (status)
+            {
+                Console.WriteLine("value parse");
+            }
+            else
+            {
+                Console.WriteLine("faild to parse to int");
+            }
+
+            if (doubleStatus)
+            {
+                Console.WriteLine("value parse to double");
+            }
+            else
+            {
+                Console.WriteLine("faild to parse double");
+            }
+        }
+    }
+}

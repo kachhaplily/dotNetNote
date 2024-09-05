c663086653623add2f362c5be562be679bc0615f
kachhaplily <kachhaplily@gmail.com>
2024-09-05 13:35:13 +0530

    constructor type

---
+++ FirstApp/defaultConstructor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    internal class defaultConstructor
+    {
+        public defaultConstructor()
+        {
+            Console.WriteLine("default constructor called");
+            Console.ReadLine();
+
+        }
+
+        static void Main(string[] args)
+        {
+            defaultConstructor obj = new defaultConstructor();
+        }
+    }
+}
---
+++ FirstApp/staticConstructor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FirstApp
+{
+    internal class StaticConstructor
+    {
+        public static int StaticValue;
+
+        // Static constructor to initialize static members
+        static StaticConstructor()
+        {
+            StaticValue = 10;
+            Console.WriteLine("Static constructor called.");
+        }
+
+        // Instance constructor
+        public StaticConstructor()
+        {
+            Console.WriteLine("Instance constructor called.");
+            Console.WriteLine("StaticValue: " + StaticValue);
+        }
+
+        static void Main(string[] args)
+        {
+            // Create an instance of the class
+            StaticConstructor obj = new StaticConstructor();
+
+            // Keep the console open
+            Console.ReadLine();
+        }
+    }
+}

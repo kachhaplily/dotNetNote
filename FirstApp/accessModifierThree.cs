619476309da9a731f7832714d40451e2e17791d2
kachhaplily <kachhaplily@gmail.com>
2024-09-06 11:10:00 +0530

    Add detailes of access modifier and add new project

---
+++ FirstApp/accessModifierThree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    internal class accessModifierThree
+    {
+
+        static void Main(string[] args)
+        {
+            accessModifier am = new accessModifier();
+            am.test2();
+            am.test4();
+            am.test5();
+           Console.ReadLine();
+            
+
+        }
+    }
+
+    //case : consuming member of class from non child class. 
+    //can not access procted and private from this class.  
+}

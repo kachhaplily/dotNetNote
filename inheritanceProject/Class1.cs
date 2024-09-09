debdd99fac92c46d5b4bf04d4f557408f4ec6ca6
kachhaplily <kachhaplily@gmail.com>
2024-09-09 09:35:09 +0530

    inheritance concept

---
+++ inheritanceProject/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritanceProject
+{
+    internal class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("parent class called");
+        }
+        public void test1()
+        {
+            Console.WriteLine("method 1");
+        }
+
+        public void test2()
+        {
+            Console.WriteLine("method 2");
+        }
+
+        private void test4()
+        {
+            Console.WriteLine("private method is called");
+        }
+    }
+}
